79f700deff20297ae78dd08c347a567480f0bfeb
hijack <xiaohaijoe@hotmail.com>
2021-02-09 11:08:23 +0800

    fix bugs

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import ScaleView from "./ScaleView";
-import DataView from "./DataView";
 import Map from "./Map";
 
-export { ScaleView, Map, DataView };
-export default { ScaleView, Map, DataView };
+export { ScaleView, Map };
+export default { ScaleView, Map };
